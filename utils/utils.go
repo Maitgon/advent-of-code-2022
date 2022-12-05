654d9cb98d80652cc69158dce474739b0d6db7f7
Maitgon <aitor98gfgb@gmail.com>
2022-12-05 13:01:10 +0100

    Stacks in utils. 他坐出租车

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,11 @@
 
 package utils
 
+import (
+	"fmt"
+	"log"
+)
+
 // This package contains functions that could be used in multiple days
 
 // Intersects two generic slices using hash table
@@ -22,4 +27,51 @@ func Intersection[T comparable](s1, s2 []T) (inter []T) {
 // Intersects 3 generic slices
 func Intersection3[T comparable](s1, s2, s3 []T) []T {
 	return Intersection(Intersection(s1, s2), s3)
+}
+
+// Implementation of Stack
+type Stack[T any] []T
+
+func (s *Stack[T]) Push(v T) int {
+	*s = append(*s, v)
+	return len(*s)
+}
+
+func (s *Stack[T]) PushN(v []T) int {
+	*s = append(*s, v...)
+	return len(*s)
+}
+
+func (s *Stack[T]) Last() T {
+	l := len(*s)
+
+	// Upto the developer to handle an empty stack
+	if l == 0 {
+		log.Fatal("Empty Stack")
+	}
+
+	last := (*s)[l-1]
+	return last
+}
+
+func (s *Stack[T]) Pop() T {
+	removed := (*s).Last()
+	*s = (*s)[:len(*s)-1]
+
+	return removed
+}
+
+func (s *Stack[T]) PopN(n int) []T {
+	removed := make([]T, n)
+	copy(removed, (*s)[len(*s)-n:])
+	*s = (*s)[:len(*s)-n]
+
+	return removed
+}
+
+// Pointer not needed because read-only operation
+func (s Stack[T]) Values() {
+	for i := len(s) - 1; i >= 0; i-- {
+		fmt.Printf("%v ", s[i])
+	}
 }
